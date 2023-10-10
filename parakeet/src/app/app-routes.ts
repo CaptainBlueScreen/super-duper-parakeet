abefd6ddeb69a332814aaa2e9ae25338478036cc
Mason Frey <CaptainBlueScreen@users.noreply.github.com>
2023-10-10 00:25:01 +0000

    Added a workign page so I know routing is working

--- parakeet/src/app/app-routes.ts
+++ parakeet/src/app/app-routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WorkingPageComponent } from './working-page/working-page.component';
 
 export const appRoutes: Routes = [
+  { path: '', component: WorkingPageComponent }, 
   {
     path: '**',
     component: PageNotFoundComponent,
---
+++ parakeet/src/app/working-page/working-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-working-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './working-page.component.html',
+  styleUrls: ['./working-page.component.scss']
+})
+export class WorkingPageComponent {
+
+}
